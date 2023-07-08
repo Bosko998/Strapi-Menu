ed7907d74f6ebba6a072356d0028a628c402b887
Boško Nikolić <boskonikolic1998@gmail.com>
2023-07-08 13:35:21 +0200

    comit	new file:   .eslintrc.cjs comit	new file:   .gitignore comit	new file:   index.html comit	new file:   package-lock.json comit	new file:   package.json comit	new file:   public/vite.svg comit	new file:   src/App.jsx comit	new file:   src/Hero.jsx comit	new file:   src/NavLinks.jsx comit	new file:   src/Navbar.jsx comit	new file:   src/Sidebar.jsx comit	new file:   src/Submenu.jsx comit	new file:   src/assets/react.svg comit	new file:   src/context.jsx comit	new file:   src/data.jsx comit	new file:   src/index.css comit	new file:   src/main.jsx comit	new file:   vite.config.js

---
+++ src/context.jsx
@@ -0,0 +1,32 @@
+import { Children, createContext, useContext, useState } from "react";
+import App from "./App";
+
+const AppContext = createContext();
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const [pageId, setPageId] = useState(null);
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        pageId,
+        setPageId,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
